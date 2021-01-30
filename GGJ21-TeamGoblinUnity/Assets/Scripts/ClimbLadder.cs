9b7c963d0e0baff46f8a9a0fb822df92f6c5b501
Thijs <thijs.van.boesschoten@unit040.com>
2021-01-30 20:48:16 +0100

    Added ladders

---
+++ GGJ21-TeamGoblinUnity/Assets/Scripts/ClimbLadder.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbLadder : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody2D rb;
+    private float inputHorizontal;
+    private float inputVertical;
+    public float distance;
+    public LayerMask ladderLayer;
+    private bool isClimbing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        inputHorizontal = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(inputHorizontal * speed, rb.velocity.y);
+        inputVertical = Input.GetAxis("Vertical");
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, ladderLayer);
+
+        if(hitInfo.collider != null)
+        {
+            if(inputVertical > 0)
+            {
+                isClimbing = true;
+            }
+        }
+        else
+        {
+            isClimbing = false;
+            rb.gravityScale = 1;
+        }
+
+        if(isClimbing == true)
+        {
+            inputVertical = Input.GetAxis("Vertical");
+            rb.velocity = new Vector2(rb.position.x, inputVertical * speed);
+            rb.gravityScale = 0;
+        }
+    }
+}
