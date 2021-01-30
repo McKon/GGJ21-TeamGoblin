33b75b83f516e381a0d2a9d475ffd0040e2502b9
SushiChu <35846999+SushiChu@users.noreply.github.com>
2021-01-31 00:21:31 +0100

    [ADD] Menu stuff
    I made some menu shit. Some stuff is broke so I gotta take a look at it later

---
+++ GGJ21-TeamGoblinUnity/Assets/Scripts/Menu Functionality/LoadSceneOnClick.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+    public void LoadScene (int scene)
+    {
+        SceneManager.LoadScene(scene);
+
+    }
+}
---
+++ GGJ21-TeamGoblinUnity/Assets/Scripts/Menu Functionality/QuitOnClick.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour
+{
+    void Quit ()
+    {
+        Application.Quit ();
+    }
+        
+}
