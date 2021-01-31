8c175b3354b2fd54ac3327f061fb707910a019ef
SushiChu <35846999+SushiChu@users.noreply.github.com>
2021-01-31 14:39:28 +0100

    [FIX] fixing quit funtionality & which level it loads

--- GGJ21-TeamGoblinUnity/Assets/Scripts/Menu Functionality/QuitOnClick.cs
+++ GGJ21-TeamGoblinUnity/Assets/Scripts/Menu Functionality/QuitOnClick.cs
@@ -1,12 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class QuitOnClick : MonoBehaviour
 {
-    void Quit ()
+
+    public void Quit()
     {
-        Application.Quit ();
+        Application.Quit();
     }
-        
+
 }
